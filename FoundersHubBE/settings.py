da14c8f7b7df9f2fc167d266e12c5ac2dffa673e
Ankit Guddewala <ankguddewala@microsoft.com>
2024-09-17 18:59:55 +0530

    first commit

--- FoundersHubBE/settings.py
+++ FoundersHubBE/settings.py
@@ -116,6 +116,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    'munilsir/static'
+]
+ 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
